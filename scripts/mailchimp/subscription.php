26ec97a6f838253cb95b00ef89e196b8335152d2
Gustavo Seganfredo <gustavosf@gmail.com>
2014-07-23 20:03:10 -0300

    Configurado mailchimp

--- scripts/mailchimp/subscription.php
+++ scripts/mailchimp/subscription.php
@@ -4,11 +4,11 @@ require_once('./MCAPI.class.php');
 extract($_POST);
 
 // Get your API key from http://admin.mailchimp.com/account/api/
-define('MC_API_KEY', 'enter you mailchimp api key here');
+define('MC_API_KEY', '48168aa7082e0756950733214660bbfd-us8');
 
 // Get your list unique id from http://admin.mailchimp.com/lists/
 // under settings at the bottom of the page, look for unique id 
-define('MC_LIST_ID', 'enter you mailchimp unique list id');
+define('MC_LIST_ID', '4f9a6a6711');
 
 // check if email is valid 
 if ( isset($email) && validEmail($email) ) {
